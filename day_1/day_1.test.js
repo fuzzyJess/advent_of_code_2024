3654e5204ce564736193c8252d64e4fa280cd1fb
Jessica Felton <jrundle99@yahoo.com>
2024-12-04 11:38:01 +0000

    add new test to work through challenge data - tests are leaking so ended up using .only to specify one test at a time.

--- day_1/day_1.test.js
+++ day_1/day_1.test.js
@@ -1,16 +1,20 @@
 const {findDifference} = require('./day_1');
+const puzzleData = require('./refactored-data');
 
 describe("findDifference", () => {
     test("when array contains only one pair returns correct difference", () => {
-        const exampleData = [[3, 4]];
-        expect(findDifference(exampleData)).toBe(1);
+        const exampleData1 = [[3, 4]];
+        expect(findDifference(exampleData1)).toBe(1);
     });
     test("when array contains two pairs returns correct difference", () => {
-        const exampleData = [[3, 4], [4, 3]];
-        expect(findDifference(exampleData)).toBe(0);
+        const exampleData2 = [[3, 4], [4, 3]];
+        expect(findDifference(exampleData2)).toBe(0);
     });
-    test.only("when array contains multiple pairs returns correct difference", () => {
-        const exampleData = [[3, 4], [4, 3], [2, 5], [1, 3], [3, 9], [3, 3]];
-        expect(findDifference(exampleData)).toBe(11);
+    test("when array contains multiple pairs returns correct difference", () => {
+        const exampleData3 = [[3, 4], [4, 3], [2, 5], [1, 3], [3, 9], [3, 3]];
+        expect(findDifference(exampleData3)).toBe(11);
+    });
+    test.only("when array contains complete challenge data returns correct difference", () => {
+        expect(findDifference(puzzleData)).toBe(2086478);
     });
 });
